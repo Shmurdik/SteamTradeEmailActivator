a58d17bda1202daeb08edd185c12cacd9d6b6088
Sergij <shandmag@gmail.com>
2016-12-27 19:47:39 +0500

    upload

---
+++ SteamTradeEmailActivator/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SteamTradeEmailActivator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label_status = new System.Windows.Forms.Label();
+            this.label_email_counter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 53);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(1234, 489);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.TabStop = false;
+            this.webBrowser1.WebBrowserShortcutsEnabled = false;
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Font = new System.Drawing.Font("a_LCDNova", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_status.ForeColor = System.Drawing.Color.Black;
+            this.label_status.Location = new System.Drawing.Point(93, 12);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(75, 27);
+            this.label_status.TabIndex = 2;
+            this.label_status.Text = "Ready";
+            // 
+            // label_email_counter
+            // 
+            this.label_email_counter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_email_counter.Location = new System.Drawing.Point(1082, 12);
+            this.label_email_counter.Name = "label_email_counter";
+            this.label_email_counter.Size = new System.Drawing.Size(164, 13);
+            this.label_email_counter.TabIndex = 3;
+            this.label_email_counter.Text = "Activating eMails: ...";
+            this.label_email_counter.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1258, 554);
+            this.Controls.Add(this.label_email_counter);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SteamTradeEmailActivator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Label label_email_counter;
+    }
+}
+
---
+++ SteamTradeEmailActivator/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace SteamTradeEmailActivator
+{
+    public partial class Form1 : Form
+    {
+        private static readonly Assembly Assembly = Assembly.GetExecutingAssembly();
+        private static readonly string ExecutableFile = Assembly.Location;
+        private static readonly string ExecutableDirectory = Path.GetDirectoryName(ExecutableFile);
+        private int email_counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            label_status.ForeColor = Color.Blue;
+            label_status.Text = "In Progress...";
+
+            var emails = new Glob(Path.Combine(ExecutableDirectory, "*.*"));
+
+            string error_path = Path.Combine(ExecutableDirectory, "error");
+            string emails_path = Path.Combine(ExecutableDirectory, "emails");
+
+            if (!Directory.Exists(error_path))
+            {
+                Directory.CreateDirectory(error_path);
+            }
+            if (!Directory.Exists(emails_path))
+            {
+                Directory.CreateDirectory(emails_path);
+            }
+
+            string email_html = "";
+
+            foreach (var val in emails.Expand().ToList())
+            {
+                string email_body = File.ReadAllText(val.FullName);
+
+                Regex r = new Regex("(http\\:\\/\\/steamcommunity\\.com\\/email\\/TradeConfirmation\\?sparams\\=[^>' \"]+)");
+                Match m = r.Match(email_body);
+                if (m.Success)
+                {
+                    webBrowser1.Navigate(new Uri(m.Groups[1].ToString()));
+                    while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+                    {
+                        Application.DoEvents();
+                    }
+                    //MessageBox.Show(webBrowser1.Url.ToString());
+                    email_html = webBrowser1.Document.Body.InnerHtml.Replace("&amp;", "&");
+                    //MessageBox.Show(html);
+                }
+                else
+                {
+                    File.Move(val.FullName, Path.GetDirectoryName(val.FullName) + "\\error\\" + val.Name);
+                    //File.WriteAllText(Path.GetDirectoryName(val.FullName) + "\\error\\" + val.Name + ".error", email_result);
+                    MessageBox.Show("Ошибка поиска TradeConfirmation! Подсунули не тот файл... он перемещён в папку error\r\nОбязательно проверить! Не запускать снова!");
+                    label_status.ForeColor = Color.Red;
+                    label_status.Text = "Error";
+                    return;
+                }
+
+                //r = null;
+
+                //Regex r = new Regex(@"(https\:\/\/steamcommunity\.com\/tradeoffer\/[0-9]+\/confirm\?accountid\=[a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)");
+                r = new Regex("(https\\:\\/\\/steamcommunity\\.com\\/tradeoffer\\/[0-9]+\\/confirm\\?accountid\\=[^>' \"]+)");
+                m = r.Match(email_html);
+                if (m.Success)
+                {
+                    bool confirmed = false;
+                    int try_count = 0;
+                    //MessageBox.Show(m.Groups[1].ToString());
+                    while (!confirmed)
+                    {
+                        try_count++;
+                        if(try_count > 5)
+                        {
+                            File.Move(val.FullName, Path.GetDirectoryName(val.FullName) + "\\error\\" + val.Name);
+                            label_status.ForeColor = Color.Red;
+                            label_status.Text = "Error";
+                            MessageBox.Show("После 5-и попыток не удалось подтвердить обмен!\r\nФайл перемещён в папку error\r\nОбязательно проверить! Не запускать снова!");
+                            return;
+                        }
+                        webBrowser1.Navigate(new Uri(m.Groups[1].ToString()));
+                        while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+                        {
+                            Application.DoEvents();
+                        }
+                        //MessageBox.Show(webBrowser1.Url.ToString());
+                        string email_result = webBrowser1.Document.Body.InnerHtml;
+                        //MessageBox.Show(html);
+                        if (email_result.ToLower().IndexOf("<h2>обмен подтвержден</h2>", StringComparison.Ordinal) > 0 || email_result.ToLower().IndexOf("<h2>trade confirmed</h2>", StringComparison.Ordinal) > 0)
+                        {
+                            //File.WriteAllText(val.FullName + ".done", email_result);
+                            File.Delete(val.FullName);
+                            email_counter++;
+                            label_email_counter.Text = "Activating eMails: " + email_counter.ToString() + " / " + emails.Expand().Count();
+                            break;
+                        }
+                        /*else
+                        {
+                            File.Move(val.FullName, Path.GetDirectoryName(val.FullName) + "\\error\\" + val.Name);
+                            //File.WriteAllText(Path.GetDirectoryName(val.FullName) + "\\error\\" + val.Name + ".error", email_result);
+                            //MessageBox.Show("Ошибка проверки подтверждения! Сорей всего не тот язык... файл перемещён в папку error\r\nОбязательно проверить! Не запускать снова!");
+                            label_status.ForeColor = Color.Red;
+                            label_status.Text = "Error";
+                            return;
+                        }*/
+                        //webBrowser1.Dispose();
+                    }
+
+                    continue;
+                }
+                else
+                {
+                    File.Move(val.FullName, Path.GetDirectoryName(val.FullName) + "\\error\\" + val.Name);
+                    //File.WriteAllText(Path.GetDirectoryName(val.FullName) + "\\error\\" + val.Name + ".error", email_result);
+                    //MessageBox.Show("Ошибка поиска tradeoffer! Сорей всего тупит стим... файл перемещён в папку error\r\nОбязательно проверить! Не запускать снова!");
+                    label_status.ForeColor = Color.Red;
+                    label_status.Text = "Error";
+                    continue;
+                }
+            }
+            button1.Enabled = true;
+            label_status.ForeColor = Color.Green;
+            label_status.Text = "Done";
+        }
+    }
+}
